55b81f83227e12581b71a8124d86e97a0dc75ea0
mal1kc <59576495+mal1kc@users.noreply.github.com>
2023-09-17 14:34:38 +0300

    anahtar olusturma silindi

--- moe_gatherer/__init__.py
+++ moe_gatherer/__init__.py
@@ -1,3 +1,3 @@
 from moe_gatherer import arayuz  # noqa
+from moe_gatherer.moe_gatherer import main  # noqa
 from .yonetici import BotIslemYonetici  # noqa
-from .anahtar_olusturucu import anahtar_olustur, AnahtarKaynagiDXdiag, SystemInfo, cihaz_yetkilimi  # noqa
--- program_ac.py
+++ program_ac.py
@@ -3,41 +3,7 @@ import multiprocessing
 multiprocessing.freeze_support()
 
 
-def _bilgi_yazdir():
-    print("\r")
-    print("s: Bot başlat.\nd : Bot durdur. (Ekran taramasi bittikten sonra duracaktir)")
-
-
-# class _Popen(multiprocessing.forking.Popen):
-#     def __init__(self, *args, **kw):
-#         if hasattr(sys, "frozen"):
-#             # We have to set original _MEIPASS2 value from sys._MEIPASS
-#             # to get --onefile mode working.
-#             os.putenv("_MEIPASS2", sys._MEIPASS)  # type: ignore
-#         try:
-#             super(_Popen, self).__init__(*args, **kw)
-#         finally:
-#             if hasattr(sys, "frozen"):
-#                 # On some platforms (e.g. AIX) 'os.unsetenv()' is not
-#                 # available. In those cases we cannot delete the variable
-#                 # but only set it to the empty string. The bootloader
-#                 # can handle this case.
-#                 if hasattr(os, "unsetenv"):
-#                     os.unsetenv("_MEIPASS2")
-#                 else:
-#                     os.putenv("_MEIPASS2", "")
-
-
-# class Process(multiprocessing.Process):
-#     _Popen = _Popen
-
-
-def main():
-    app = uygulama_olustur()
-    app.mainloop()
-
-
 if __name__ == "__main__":
-    from moe_gatherer import uygulama_olustur
+    from moe_gatherer import main
 
     main()
